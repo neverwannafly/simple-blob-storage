11e4f49b73a443b1c7d8254166655f50c3e8468a
Shubham Anand <shubham_chess@live.com>
2021-04-28 17:16:34 +0530

    create basic pages

--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -1,3 +1,11 @@
 class AuthToken < ApplicationRecord
   belongs_to :bearer, polymorphic: true
+  encrypts :token, deterministic: true
+
+  enum status: [:active, :inactive]
+  enum scope: [:read_only, :write_only, :read_and_write]
+
+  def self.authenticate(token, scope: :read_and_write)
+    return self.where(token: token, scope: scope).first.present?
+  end
 end
