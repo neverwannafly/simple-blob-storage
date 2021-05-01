ff2293dc412cfe36e189b91da20dffe84ef9e828
Shubham Anand <shubham_chess@live.com>
2021-05-01 17:30:34 +0530

    indentation fixes

--- app/javascript/packs/utils.js
+++ app/javascript/packs/utils.js
@@ -9,7 +9,7 @@ export function whenDocReady(fn) {
 export function searchParams(params) {
   const paramsList = [];
   Object.keys(params).forEach((k) => {
-      paramsList.push(`${k}=${params[k]}`);
+    paramsList.push(`${k}=${params[k]}`);
   });
   return paramsList.join('&');
 }
@@ -17,56 +17,56 @@ export function searchParams(params) {
 export async function parseJsonResponse(response) {
   let json = null;
   try {
-      json = await response.json();
+    json = await response.json();
   } catch (e) {
-      // TODO Do something if response has no, or invalid JSON
+    // TODO Do something if response has no, or invalid JSON
   }
 
   if (response.ok) {
-      return json;
+    return json;
   } else {
-      let error = new Error(response.statusText);
-      error.isFromServer = true;
-      error.response = response;
-      error.responseJson = json;
+    let error = new Error(response.statusText);
+    error.isFromServer = true;
+    error.response = response;
+    error.responseJson = json;
 
-      throw error;
+    throw error;
   }
 }
 
 export async function apiRequest(method, path, body = null, options = {}) {
   let defaultHeaders = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'X-Requested-With': "XMLHttpRequest"
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'X-Requested-With': "XMLHttpRequest"
   };
 
   // TODO Remove DOM dependency from this file
   const csrfMeta = document.querySelector('meta[name="csrf-token"]');
   if (csrfMeta) {
-      defaultHeaders['X-CSRF-Token'] = csrfMeta.content;
+    defaultHeaders['X-CSRF-Token'] = csrfMeta.content;
   }
 
   let defaultOptions = { method };
   if (body) {
-      if (options.dataType === 'FormData') {
-          delete defaultHeaders['Content-Type'];
-          defaultOptions['body'] = body;
-      } else {
-          defaultOptions['body'] = JSON.stringify(body);
-      }
+    if (options.dataType === 'FormData') {
+      delete defaultHeaders['Content-Type'];
+      defaultOptions['body'] = body;
+    } else {
+      defaultOptions['body'] = JSON.stringify(body);
+    }
   }
 
   const { headers, params, ...remainingOptions } = options;
   const finalOptions = Object.assign(
-      defaultOptions,
-      { headers: Object.assign(defaultHeaders, headers) },
-      { credentials: 'same-origin' },
-      remainingOptions
+    defaultOptions,
+    { headers: Object.assign(defaultHeaders, headers) },
+    { credentials: 'same-origin' },
+    remainingOptions
   );
 
   if (params) {
-      path += `?${searchParams(params)}`;
+    path += `?${searchParams(params)}`;
   }
 
   const response = await fetch(path, finalOptions);
