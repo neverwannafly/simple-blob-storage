47759723ec1ad719bff4422cd9748c9cfc12361a
Shubham Anand <shubham_chess@live.com>
2021-05-03 19:37:16 +0530

    remove redundant code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:user) do |u|
-      u.permit(:username, :email, :password, :password_confirmation, :commit)
-    end
-  end
 end
